a75b58f7d4869b0caa0052b451bfbcb9a512c257
Anuar77 <46727468+Anuar77@users.noreply.github.com>
2022-06-03 16:36:33 +0500

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Anuar Aitkali on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
